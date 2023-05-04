081a93d7f07f7b2706b8931d5c14ec2917d9f626
stephenkati <stephenkatuli88@gmail.com>
2023-05-04 16:05:12 +0300

    create Addbook component

---
+++ src/components/AddBook.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Book title..." />
+      <input type="text" placeholder="Book Author" />
+      <input type="submit" />
+    </form>
+  </div>
+);
+
+export default AddBook;
