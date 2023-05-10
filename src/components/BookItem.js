d771b00c4127a17349df153b11be90ddbc054ca6
stephenkati <stephenkatuli88@gmail.com>
2023-05-10 15:22:30 +0300

    add new content, refactor to accomodate styles

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { deleteBook } from '../redux/books/BookSlice';
 
 const BookItem = (props) => {
-  const { id, title, author } = props;
+  const {
+    id,
+    title,
+    author,
+  } = props;
+
   const dispatch = useDispatch();
+  const [progress, setProgress] = useState(50);
+
+  const handlePercentage = () => {
+    let percentage = progress;
+    if (percentage < 100) {
+      setProgress(percentage += 1);
+    }
+  };
 
   return (
-    <li>
-      <div className="book">
-        <span className="title">{title}</span>
-        <span className="author">{author}</span>
+    <li className="book">
+      <div className="detail-actions">
+        <div className="bookDetails">
+          <span className="category">Fiction</span>
+          <span className="title">{title}</span>
+          <span className="author">{author}</span>
+        </div>
+        <div className="actions">
+          <button type="button" className="comment actionBtn">Comments</button>
+          <button
+            type="submit"
+            className="removeBook actionBtn"
+            onClick={() => dispatch(deleteBook(id))}
+          >
+            Remove
+          </button>
+          <button type="button" className="actionBtn">Edit</button>
+        </div>
       </div>
-      <button
-        type="submit"
-        className="removeBook"
-        onClick={() => dispatch(deleteBook(id))}
-      >
-        Remove
-      </button>
+      <div className="percentage">
+        <CircularProgressbar
+          value={progress}
+          className="progressBar"
+        />
+        <div className="numberPercentage">
+          <span>
+            {progress}
+            %
+          </span>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div className="progressSection">
+        <h2 className="currentChapter">CURRENT CHAPTER</h2>
+        <p className="chapter">Chapter 5</p>
+        <button
+          type="button"
+          onClick={handlePercentage}
+          className="updateProgress"
+        >
+          UPDATE PROGRESS
+        </button>
+      </div>
     </li>
   );
 };
