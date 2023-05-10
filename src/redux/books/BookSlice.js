8ea9ffd63cd9fc8c87ff2963cbc3b8fb19fe9bf2
stephenkati <stephenkatuli88@gmail.com>
2023-05-10 04:10:25 +0300

    update initial state

--- src/redux/books/BookSlice.js
+++ src/redux/books/BookSlice.js
@@ -1,6 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const BooksSlice = createSlice({
   name: 'books',
@@ -8,11 +27,9 @@ const BooksSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       state.push(action.payload);
+      return state;
     },
-    removeBook: (state, action) => {
-      const itemId = action.payload;
-      state.books = state.books.filter((item) => item.id !== itemId);
-    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
   },
 });
 
