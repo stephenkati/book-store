307bb995c148909dd30d20ea209616324ff8ca65
stephenkati <stephenkatuli88@gmail.com>
2023-05-10 15:21:09 +0300

    update Navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../App.css';
+import { MdPerson } from 'react-icons/md';
 
 const Navbar = () => (
   <nav>
-    <h1>Bookstore CMS</h1>
+    <h1 className="header">Bookstore CMS</h1>
     <ul>
       <li>
-        <Link to="/" className="link">Books</Link>
-      </li>
-      <li>
+        <Link to="/" className="link"> Books </Link>
         <Link to="/categories" className="link"> Categories </Link>
       </li>
     </ul>
+    <div className="oval">
+      <MdPerson className="profile" />
+    </div>
   </nav>
 );
 
