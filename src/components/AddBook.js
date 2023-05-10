1936ae66480d6fd620cde16411365eff6b65c1f3
stephenkati <stephenkatuli88@gmail.com>
2023-05-10 04:09:04 +0300

    update functionalty for adding new book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/BookSlice';
 
-const AddBook = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="Book title..." />
-      <input type="text" placeholder="Book Author" />
-      <input type="submit" />
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = () => {
+    const bookInfo = {
+      item_id: uuidv4(),
+      title,
+      author,
+      category: 'Fiction',
+    };
+    if (title && author) {
+      dispatch(addBook(bookInfo));
+    }
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div>
+      <form id="form">
+        <input
+          type="text"
+          placeholder="Book title..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Book Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(handleSubmit);
+          }}
+        >
+          ADD BOOK
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
